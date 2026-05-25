ac5e5a515d9152bedc145832322f4ae5698ce8b9
Chris Connett <chrisconnett@gmail.com>
2026-05-25 19:36:28 -0400

    Complete implementation of tiebreaker options.

--- voting/mam.py
+++ voting/mam.py
@@ -8,11 +8,19 @@ import networkx
 
 
 class Tiebreaker(enum.Enum):
+    # Return ties as sets.
+    NONE = 0
+    # Random Voter Hierarchy: Break ties with the preference of a random
+    # ballot, cascading to additional ballots when the tie remains
+    # unresolved. Ties are still possible if no voter expresses any preference
+    # between two candidates; ties are returned as sets.
     RVH = 1
+    # As RVH, but break any remaining ties with a random total ordering;
+    # returns a total ordering of candidates.
     LINEAR = 2
 
 
-def MaximizeAffirmedMajorities(ballots):
+def MaximizeAffirmedMajorities(ballots, /, tiebreaker=Tiebreaker.RVH, seed=None):
     # Normalize ballots by wrapping naked entries in a singleton tuple.
     ballots = [
         [(rank,) if not isinstance(rank, tuple) else rank for rank in ballot]
@@ -25,17 +33,27 @@ def MaximizeAffirmedMajorities(ballots):
         for rank in ballot:
             candidates |= set(rank)
 
+    for ballot in ballots:
+        unranked = set(candidates)
+        for rank in ballot:
+            unranked -= set(rank)
+        if unranked:
+            ballot.append(tuple(sorted(unranked)))
+
+    seeded_random = random.Random(seed)
+    linear_breaker = sorted(candidates)
+    random.Random(seeded_random.randbytes(32)).shuffle(linear_breaker)
+    ballots = sorted(ballots)
+    seeded_random.shuffle(ballots)
+
     # Table of count of the pairwise preferences.
     preferences = collections.defaultdict(collections.Counter)
 
-    # Shuffle the ballots to later create a tiebreak ordering using random voter
-    # hierarchy.
-    random.shuffle(ballots)
-
-    # Make a tiebreak ordering that will be a total linear ordering after processing all
-    # ballots.
-    tiebreak_ordering = networkx.DiGraph()
-    tiebreak_ordering.add_nodes_from(candidates)
+    # Make a tiebreak ordering that will ideally be a total linear ordering
+    # after processing all ballots. (If it is not, some candidates may be tied
+    # in the final ranking, or ties broken by chance.)
+    tiebreak_graph = networkx.DiGraph()
+    tiebreak_graph.add_nodes_from(candidates)
 
     # Two things happen in the main counting loop.
     for ballot in ballots:
@@ -44,20 +62,25 @@ def MaximizeAffirmedMajorities(ballots):
                 for a, b in itertools.product(higher_rank, lower_rank):
                     # 1. Add each of this ballot's strict preferences to the tiebreak
                     # ordering.
-                    if not networkx.has_path(tiebreak_ordering, b, a):
-                        tiebreak_ordering.add_edge(a, b)
+                    if tiebreaker != Tiebreaker.NONE and not networkx.has_path(
+                        tiebreak_graph, b, a
+                    ):
+                        tiebreak_graph.add_edge(a, b)
                     # 2. Add the strict preference in the preference table.
                     preferences[a][b] += 1
-    logging.debug(preferences)
 
-    # In pathological cases, the tiebreak ordering might be incomplete. Complete the
-    # tiebreak ordering with a random order.
-    final_breaker = list(candidates)
-    random.shuffle(final_breaker)
-    for a, b in zip(final_breaker, final_breaker[1:]):
-        if not networkx.has_path(tiebreak_ordering, b, a):
-            tiebreak_ordering.add_edge(a, b)
-    tiebreak_ordering = list(networkx.topological_sort(tiebreak_ordering))
+    if tiebreaker == Tiebreaker.LINEAR:
+        for a, b in itertools.product(candidates, candidates):
+            if linear_breaker.index(a) < linear_breaker.index(
+                b
+            ) and not networkx.has_path(tiebreak_graph, b, a):
+                tiebreak_graph.add_edge(a, b)
+    tiebreak_ranks = {}
+    for index, generation in enumerate(
+        networkx.topological_generations(tiebreak_graph)
+    ):
+        tiebreak_ranks.update({candidate: index for candidate in generation})
+    logging.debug(tiebreak_ranks)
 
     # We are now ready to construct the final order.
     final_order = networkx.DiGraph()
@@ -65,38 +88,55 @@ def MaximizeAffirmedMajorities(ballots):
 
     pairwise_defeats = []
     for a, b in itertools.product(candidates, candidates):
-        if a != b:
+        if a != b and preferences[a][b] > preferences[b][a]:
             pairwise_defeats.append(
                 (
                     (a, b),
-                    (preferences[a][b] - preferences[b][a], preferences[a][b]),
+                    (
+                        preferences[a][b],
+                        -preferences[b][a],
+                        tiebreak_ranks[b],
+                        -tiebreak_ranks[a],
+                    ),
                 )
             )
     # Sort the pairwise defeats first by majority minus minority, then majority.
     pairwise_defeats.sort(key=lambda e: e[1], reverse=True)
     # Apply each pairwise defeat to the final order. Group by the metric value
     # so tied groups are processed together.
-    for _, group in itertools.groupby(pairwise_defeats, key=lambda e: e[1]):
+    for metric, group in itertools.groupby(pairwise_defeats, key=lambda e: e[1]):
         group = list(group)
-        # The group tie is broken by (1) which pair's defeated candidate is the lowest
-        # on the tiebreak order; then (2) which pair's winning candidate is highest in
-        # the tiebreak order.
-        group.sort(
-            key=lambda e: (
-                -tiebreak_ordering.index(e[0][1]),
-                tiebreak_ordering.index(e[0][0]),
-            )
-        )
-        # Apply the pairwise defeat.
+        if metric[0] <= 0:
+            # The first metric of this group is negative. We've crossed the
+            # midway point and would now be processing inverses of defeats
+            # already added. We're done.
+            break
+        if len(group) > 1:
+            logging.debug("Probing application of tied group")
+        # Apply the pairwise defeat. If every edge in this group applies
+        # cleanly, it is kept. Otherwise, the group is hopelessly tied and must
+        # be ignored.
+        probe = final_order.copy()
         for (a, b), metric in group:
-            if not networkx.has_path(final_order, b, a):
+            if not networkx.has_path(probe, b, a):
                 logging.debug(f"Applying {a} > {b} : {metric}")
-                final_order.add_edge(a, b)
+                probe.add_edge(a, b)
             else:
                 logging.debug(f"Cannot apply {a} > {b} : {metric}")
+                probe = final_order
+                break
+        final_order = probe
 
-    # Finally, apply the tiebreak ordering to resolve any other unresolved loops.
-    for a, b in zip(tiebreak_ordering, tiebreak_ordering[1:]):
-        if not networkx.has_path(final_order, b, a):
-            final_order.add_edge(a, b)
-    return list(networkx.topological_sort(final_order))
+    logging.debug(tiebreak_ranks)
+    if tiebreaker != Tiebreaker.NONE:
+        # Finally, apply the tiebreak ordering to resolve any other unresolved loops.
+        for a, b in itertools.product(candidates, candidates):
+            if tiebreak_ranks[a] < tiebreak_ranks[b] and not networkx.has_path(
+                final_order, b, a
+            ):
+                final_order.add_edge(a, b)
+    generations = list(networkx.topological_generations(final_order))
+    return [
+        generation[0] if len(generation) == 1 else set(generation)
+        for generation in generations
+    ]
--- voting/mam_test.py
+++ voting/mam_test.py
@@ -7,18 +7,18 @@ class MamTest(unittest.TestCase):
 
     def test_basicballot(self):
         self.assertEqual(
-            ['a', 'b', 'c'], mam.MaximizeAffirmedMajorities([['a', 'b', 'c']])
+            ["a", "b", "c"], mam.MaximizeAffirmedMajorities([["a", "b", "c"]])
         )
 
     def test_tupleballot(self):
         self.assertEqual(
-            ['a', 'b', 'c'],
+            ["a", "b", "c"],
             mam.MaximizeAffirmedMajorities(
                 [
                     [
-                        ('a',),
-                        ('b',),
-                        ('c'),
+                        ("a",),
+                        ("b",),
+                        ("c"),
                     ]
                 ]
             ),
@@ -26,70 +26,143 @@ class MamTest(unittest.TestCase):
 
     def test_unanimity(self):
         self.assertEqual(
-            ['a', 'b', 'c'],
+            ["a", "b", "c"],
             mam.MaximizeAffirmedMajorities(
                 [
-                    ['a', 'b', 'c'],
-                    ['a', 'b', 'c'],
+                    ["a", "b", "c"],
+                    ["a", "b", "c"],
                 ]
             ),
         )
 
     def test_outvoted(self):
         self.assertEqual(
-            ['c', 'b', 'a'],
+            ["c", "b", "a"],
             mam.MaximizeAffirmedMajorities(
                 [
-                    ['a', 'b', 'c'],
-                    ['c', 'b', 'a'],
-                    ['c', 'b', 'a'],
+                    ["a", "b", "c"],
+                    ["c", "b", "a"],
+                    ["c", "b", "a"],
                 ]
             ),
         )
 
     def test_ambivalence(self):
         self.assertEqual(
-            ['a', 'b', 'c'],
+            ["a", "b", "c"],
             mam.MaximizeAffirmedMajorities(
                 [
-                    ['a', ('b', 'c')],
-                    [('a', 'b'), 'c'],
+                    ["a", ("b", "c")],
+                    [("a", "b"), "c"],
                 ]
+            ),
+        )
+
+    def test_full_tie(self):
+        self.assertEqual(
+            [{"a", "b", "c"}, {"d", "e"}],
+            mam.MaximizeAffirmedMajorities(
+                [
+                    ["a", "b", "c"],
+                    ["b", "c", "a"],
+                    ["c", "a", "b"],
+                    [("d", "e")],
+                    [("e", "d")],
+                ],
+                tiebreaker=mam.Tiebreaker.NONE,
+            ),
+        )
+
+    def test_broken_tie_RVH(self):
+        self.assertEqual(
+            ["a", "b", "c", {"d", "e"}],
+            mam.MaximizeAffirmedMajorities(
+                [
+                    ["a", "b", "c"],
+                    ["b", "c", "a"],
+                    ["c", "a", "b"],
+                    [("d", "e")],
+                    [("e", "d")],
+                ],
+                tiebreaker=mam.Tiebreaker.RVH,
+                seed=1,
+            ),
+        )
+        self.assertEqual(
+            ["c", "a", "b", {"d", "e"}],
+            mam.MaximizeAffirmedMajorities(
+                [
+                    ["a", "b", "c"],
+                    ["b", "c", "a"],
+                    ["c", "a", "b"],
+                    [("d", "e")],
+                    [("e", "d")],
+                ],
+                tiebreaker=mam.Tiebreaker.RVH,
+                seed=5,
+            ),
+        )
+
+    def test_broken_tie_LINEAR(self):
+        self.assertEqual(
+            ["a", "b", "c", "d", "e"],
+            mam.MaximizeAffirmedMajorities(
+                [
+                    ["a", "b", "c"],
+                    ["b", "c", "a"],
+                    ["c", "a", "b"],
+                    [("d", "e")],
+                    [("e", "d")],
+                ],
+                tiebreaker=mam.Tiebreaker.LINEAR,
+                seed=1,
+            ),
+        )
+        self.assertEqual(
+            ["c", "a", "b", "d", "e"],
+            mam.MaximizeAffirmedMajorities(
+                [
+                    ["a", "b", "c"],
+                    ["b", "c", "a"],
+                    ["c", "a", "b"],
+                    [("d", "e")],
+                    [("e", "d")],
+                ],
+                tiebreaker=mam.Tiebreaker.LINEAR,
+                seed=5,
             ),
         )
 
     def test_known_outcome(self):
         self.assertEqual(
-            ['b', 'c', 'a'],
+            ["b", "c", "a"],
             mam.MaximizeAffirmedMajorities(
-                [['a', 'b', 'c']] * 36
-                + [['b', 'a', 'c']] * 12
-                + [['b', 'c', 'a']] * 8
-                + [['c', 'b', 'a']] * 44
+                [["a", "b", "c"]] * 36
+                + [["b", "a", "c"]] * 12
+                + [["b", "c", "a"]] * 8
+                + [["c", "b", "a"]] * 44
             ),
         )
 
     def test_rps(self):
         self.assertEqual(
-            ['r', 's', 'p'],
+            ["r", "s", "p"],
             mam.MaximizeAffirmedMajorities(
-                [['r', 's', 'p']] * 40
-                + [['s', 'p', 'r']] * 35
-                + [['p', 'r', 's']] * 25
+                [["r", "s", "p"]] * 40 + [["s", "p", "r"]] * 35 + [["p", "r", "s"]] * 25
             ),
         )
 
     def test_wikipedia_tennesee(self):
         self.assertEqual(
-            ['nashville', 'chattanooga', 'knoxville', 'memphis'],
+            ["nashville", "chattanooga", "knoxville", "memphis"],
             mam.MaximizeAffirmedMajorities(
-                [['memphis', 'nashville', 'chattanooga', 'knoxville']] * 42
-                + [['nashville', 'chattanooga', 'knoxville', 'memphis']] * 26
-                + [['chattanooga', 'knoxville', 'nashville', 'memphis']] * 15
-                + [['knoxville', 'chattanooga', 'nashville', 'memphis']] * 17
+                [["memphis", "nashville", "chattanooga", "knoxville"]] * 42
+                + [["nashville", "chattanooga", "knoxville", "memphis"]] * 26
+                + [["chattanooga", "knoxville", "nashville", "memphis"]] * 15
+                + [["knoxville", "chattanooga", "nashville", "memphis"]] * 17
             ),
         )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
