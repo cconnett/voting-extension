b6a5f0a41c4bfd118c6d442e88eeac53f9889444
Chris Connett <chrisconnett@gmail.com>
2026-06-01 15:29:02 -0400

    Add more endpoint implementations.

--- backend/flask_app.py
+++ backend/flask_app.py
@@ -1,6 +1,7 @@
 import contextlib
 import json
 import os.path
+import random
 import sqlite3
 import uuid
 
@@ -45,13 +46,15 @@ def hello():
 @app.route("/create", methods=["POST"])
 def create():
     new_id = uuid.uuid4()
+    salt = uuid.uuid4()
     channel_id = flask.request.form["channel_id"]
     title = flask.request.form["title"]
     candidates = flask.request.form["candidates"]
     with db_connection() as conn:
         conn.execute(
-            "INSERT INTO polls VALUES (?, ?, ?, ?)",
+            "INSERT INTO polls VALUES (?, ?, ?, ?, ?)",
             new_id.int,
+            salt.int,
             channel_id,
             title,
             candidates,
@@ -74,16 +77,64 @@ def close(poll_id):
 
 @app.route("/results/<uuid:poll_id>", methods=["GET"])
 def results(poll_id):
-    pass
+    import mam
+
+    with db_connection() as conn:
+        cur = conn.cursor()
+        cur.arraysize = 16
+        cur.execute("SELECT candidates, salt, open FROM polls WHERE id=?", poll_id.int)
+        candidate_string, salt, is_open = cur.fetchone()
+        cur = conn.execute("SELECT ranking FROM ballots WHERE poll_id=?", poll_id.int)
+        ranking_rows = cur.fetchall()
+    candidates = json.loads(candidate_string)
+    ballots = []
+    for row in ranking_rows:
+        ranking_dict = json.loads(row[0])
+        ballots.append(sorted(ranking_dict.items(), key=lambda pair: pair[1]))
+    ordering, defeat_matrix = mam.MaximizeAffirmedMajorities(
+        ballots,
+        tiebreaker=mam.Tiebreaker.NONE if is_open else mam.Tiebreaker.LINEAR,
+        seed=salt,
+    )
+
+
+@app.route("/results/winner/<uuid:poll_id>", methods=["GET"])
+def winner(poll_id):
+    ordering, matrix = results(poll_id)
+    flask.response.content_type = "text/json"
+    return json.dumps(ordering[0])
 
 
 @app.route("/ballot/<uuid:poll_id>", methods=["GET"])
 def ballot(poll_id):
+    token = flask.request.form["token"]  # FIXME
     with db_connection() as conn:
-        cur = conn.execute("SELECT candidates from polls WHERE id=?", poll_id.int)
+        cur = conn.execute(
+            "SELECT title, candidates FROM polls WHERE id=?", poll_id.int
+        )
         row = cur.fetchone()
-        candidates = json.loads(row[0])
-    return flask.render_template("templates/ballot.html", candidates=candidates)
+        if not row:
+            abort(404)
+        cur = conn.execute(
+            "SELECT ranking FROM ballots WHERE poll_id=? AND opaque_user_id=?",
+            poll_id.int,
+            token,
+        )
+        ranking = cur.fetchone()
+    title, candidates_string = row[0]
+    candidates = json.loads(candidates_string)
+    random.shuffle(candidates)
+    if ranking:
+        ranking = json.loads(ranking[0])
+        for candidate in ranking:
+            candidates.remove(candidate)
+
+    return flask.render_template(
+        "templates/ballot.html",
+        title=title,
+        candidates=candidates,
+        ranking=ranking,
+    )
 
 
 @app.route("/cast_vote/<uuid:poll_id>", methods=["POST"])
