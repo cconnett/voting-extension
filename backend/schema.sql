4797b8ad2f99d661196ccf4ea2f67972ed8736f9
Chris Connett <chrisconnett@gmail.com>
2026-06-01 22:39:58 -0400

    Add db schema.

---
+++ backend/schema.sql
@@ -0,0 +1,16 @@
+CREATE TABLE polls (
+    id INTEGER PRIMARY KEY,
+    salt INTEGER,
+    channel_id TEXT NOT NULL,
+    title TEXT,
+    candidates TEXT NOT NULL,  -- JSON array
+    open INTEGER DEFAULT 1,
+    created_at TEXT DEFAULT (datetime('now'))
+);
+CREATE TABLE ballots (
+    poll_id INTEGER NOT NULL REFERENCES polls(id),
+    opaque_user_id TEXT NOT NULL,
+    ranking TEXT NOT NULL, -- flat JSON array of ranked candidates
+    cast_at TEXT DEFAULT (datetime('now')),
+    PRIMARY KEY (poll_id, opaque_user_id)
+) WITHOUT ROWID;
